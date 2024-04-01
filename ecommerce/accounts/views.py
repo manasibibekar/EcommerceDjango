5dcc9faeea6059d5ef25aaa4824c6d28e7423516
MANASI BIBEKAR <manasi.bibekar@adityabirlacapital.com>
2024-04-01 17:06:08 +0530

    register feature | register new user and throw error if the username already exists. username is email id

--- ecommerce/accounts/views.py
+++ ecommerce/accounts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def login_page(request):
@@ -6,4 +8,26 @@ def login_page(request):
 
 
 def register_page(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if User.objects.filter(username=email).exists():
+            messages.warning(request, 'Email is already taken.')
+            return HttpResponseRedirect(request.path_info)
+        
+        user_obj = User.objects.create(
+            username = email,
+            first_name = first_name,
+            last_name = last_name,
+            email = email
+        )
+        user_obj.set_password(password)
+        user_obj.save()
+
+        messages.info(request, 'An email has been sent on your mail')
+        return HttpResponseRedirect(request.path_info)
+    
     return render(request, 'accounts/register.html')
