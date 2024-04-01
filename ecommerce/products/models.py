c58e4fd90accd688ed7a423058f96d79b776e233
MANASI BIBEKAR <manasi.bibekar@adityabirlacapital.com>
2024-04-01 15:05:36 +0530

    products model and base model

--- ecommerce/products/models.py
+++ ecommerce/products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from base.models import BaseModel
 
-# Create your models here.
+
+class Category(BaseModel):
+    category_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    category_image = models.ImageField(upload_to='categories')
+
+
+class Product(BaseModel):
+    product_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE, related_name='product_related')
+    price = models.IntegerField()
+    product_description = models.TextField()
+
+
+class ProductImage(BaseModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_image_related')
+    image = models.ImageField(upload_to='product')
